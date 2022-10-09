21a5ee54b52646f241b57dc9cbd93d357231a944
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-10-09 11:37:34 +0200

    main

--- database/migrations/2022_10_01_163323_create_links_table.php
+++ database/migrations/2022_10_01_163323_create_links_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('link');
             $table->timestamps();
         });
